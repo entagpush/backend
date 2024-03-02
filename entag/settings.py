526bd74a1f15ab59336cb446dace190843153e96
dunmininu <alakaoluwaseyi@gmail.com>
2024-03-02 18:39:51 +0100

    push staticfiles

--- entag/settings.py
+++ entag/settings.py
@@ -184,7 +184,7 @@ SWAGGER_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = config("STATIC_URL", "/static/")
 STATIC_ROOT = PROJECT_ROOT / "staticfiles"
 
 
