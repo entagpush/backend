ba9dbfc0b02c02e4746368d62524710ec0061a85
dunmininu <alakaoluwaseyi@gmail.com>
2024-05-26 14:32:59 +0100

    chore: added the urls

--- entag/urls.py
+++ entag/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("website.urls")),
     path("", include("accounts.urls")),
+    path("", include("customer.urls")),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
